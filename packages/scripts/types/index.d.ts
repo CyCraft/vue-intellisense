07389dc4671a6035032f7117d98e61a0de85c46d
Luca Ban <mesqueeb@gmail.com>
2020-09-20 10:12:48 +0900

    chore: build

--- packages/scripts/types/index.d.ts
+++ packages/scripts/types/index.d.ts
@@ -1,2 +1,4 @@
 export declare function vueFilePathToVeturJsonData(vueFilePath: string, veturFile: 'attributes' | 'tags'): Promise<Record<string, any>>;
-export declare function generateVeturFiles(inputPath: string, outputPath: string): Promise<void>;
+export declare function generateVeturFiles(inputPath: string, outputPath: string, options?: {
+    recursive?: boolean;
+}): Promise<void>;
--- packages/scripts/types/listFiles.d.ts
+++ packages/scripts/types/listFiles.d.ts
@@ -1,7 +1,19 @@
 /**
  * @param {string} folderPath "resources/foo/goo"
- * @param {RegExp} regexFilter eg. /\.txt/
- * @param {boolean} resolvePaths when true it will return the _full_ path from the file system's root. If false (default) it will return the relativePath based on the initial directory path passed.
+ * @param {{
+    regexFilter?: RegExp,
+    resolvePaths?: boolean,
+    recursive?: boolean,
+  }} options
+ * regexFilter: RegExp - eg. /\.txt/ for only .txt files
+ *
+ * resolvePaths: boolean - when true it will return the _full_ path from the file system's root. If false (default) it will return the relativePath based on the initial directory path passed
+ *
+ * recursive: boolean - when true it will return ALL paths recursively. If false (default) it will only return the paths from the folder
  * @return {Promise<string[]>}
  */
-export declare function listFiles(folderPath: string, regexFilter?: RegExp, resolvePaths?: boolean): Promise<string[]>;
+export declare function listFiles(folderPath: string, options?: {
+    regexFilter?: RegExp;
+    resolvePaths?: boolean;
+    recursive?: boolean;
+}): Promise<string[]>;
